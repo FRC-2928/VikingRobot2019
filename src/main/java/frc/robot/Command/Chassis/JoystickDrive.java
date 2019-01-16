52d6b270f97e898fa3f500bc1919631dcdb720fa
JettK <jett.kim@gmail.com>
2019-01-16 15:38:51 -0800

    Almost driveable
    Stripped down to drivetrain, almost driveable

--- src/main/java/frc/robot/Command/Chassis/JoystickDrive.java
+++ src/main/java/frc/robot/Command/Chassis/JoystickDrive.java
@@ -1,32 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.Command.Chassis;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Robot;
 
 public class JoystickDrive extends Command {
+  public JoystickDrive() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
 
-    @Override
-    protected boolean isFinished() {
-        return !DriverStation.getInstance().isOperatorControl();
-    }
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
 
-    public JoystickDrive()
-    {
-        requires(Robot.chassis.drivetrain);
-    }
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
 
-    public void initialize()
-    {
-        Robot.chassis.drivetrain.setMotorSafetyEnabled(true);
-    }
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 
-    public void execute() {
-        double driveX = Robot.oi.getDriveX();
-        double driveY = Robot.oi.getDriveY();
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
 
-        System.out.println("driving");
-        Robot.chassis.drivetrain.drive(driveY, driveX);
-    }
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,18 +1,18 @@
-package frc.robot;
+ package frc.robot;
 
-public class RobotMap {
+// public class RobotMap {
 
-    //Talon SRX
-    public static final int TALON_FRONT_LEFT = 15;
-    public static final int TALON_FRONT_RIGHT = 0;
-    public static final int TALON_BACK_LEFT = 14;
-    public static final int TALON_BACK_RIGHT = 1;
+//     //Talon SRX
+//     public static final int TALON_FRONT_LEFT = 15;
+//     public static final int TALON_FRONT_RIGHT = 0;
+//     public static final int TALON_BACK_LEFT = 14;
+//     public static final int TALON_BACK_RIGHT = 1;
 
 
 
-    public static final int PIGEON = 3;
+//     public static final int PIGEON = 3;
 
-    //Solenoids
-    public static final int SOLENOID_TRANSMISSION_LOW = 1;
-    public static final int SOLENOID_TRANSMISSION_HIGH = 2;
-}
+//     //Solenoids
+//     public static final int SOLENOID_TRANSMISSION_LOW = 1;
+//     public static final int SOLENOID_TRANSMISSION_HIGH = 2;
+// }
