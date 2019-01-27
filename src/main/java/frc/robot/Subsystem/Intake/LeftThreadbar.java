1bcc279a2ed179b8cf809db8bc487a878084527c
matthewpanc <44793372+matthewpanc@users.noreply.github.com>
2019-01-27 14:50:49 -0800

    LeftThreadbar
    Left Threadbar encoder work, not fully working

--- src/main/java/frc/robot/Subsystem/Intake/LeftThreadbar.java
+++ src/main/java/frc/robot/Subsystem/Intake/LeftThreadbar.java
@@ -6,37 +6,30 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
-/**
- * Add your docs here.
- */
+
 public class LeftThreadbar extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
   
   //motor setup
   public WPI_TalonSRX leftThreadbarMotor;
+  
+  //encoder setup
+  public Encoder leftEncoder;
 
   public LeftThreadbar(){
     
+    //Setting up the threadbar motor
     leftThreadbarMotor = new WPI_TalonSRX(RobotMap.TALON_LEFT_THREADBAR);
-
-  }
-
-  //encoder setup
-  Encoder leftEncoder;
-  //DigitalInput leftThreadbarInnerLimit; //limitswitch on inside of the left threadbar arm
-  //DigitalInput leftThreadbarOuterLimit; //limitswitch on outside of the left threadbar arm
-  
-  public void Init(){
 
     leftEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 
-    //leftThreadbarInnerLimit = new DigitalInput(1);
-    //leftThreadbarOuterLimit = new DigitalInput(2);
-
   }
+
+
+
 
   public void setLeftPower(double power){
 
@@ -45,14 +38,23 @@ public class LeftThreadbar extends Subsystem {
   }
 
   public void setLeftEncoderTicks(double intake){
-
     intake = leftEncoder.get();
+    System.out.println(leftEncoder.get());
+  }
+
+  public void getLeftEncoder(){
+
+    leftEncoder.get();
+
+  }
+
+  public void resetLeftEncoder(){
+
+    leftEncoder.reset();
 
   }
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
   }
 }
