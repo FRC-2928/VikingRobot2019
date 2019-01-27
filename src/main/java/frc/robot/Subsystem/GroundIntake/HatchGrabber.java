eb8f80a08a95dde1673c26bca1898ff98182b9a9
matthewpanc <44793372+matthewpanc@users.noreply.github.com>
2019-01-27 13:35:35 -0800

    sensor bar
    contrubeted to sensor bar setup

--- src/main/java/frc/robot/Subsystem/GroundIntake/HatchGrabber.java
+++ src/main/java/frc/robot/Subsystem/GroundIntake/HatchGrabber.java
@@ -1,53 +1,30 @@
 package frc.robot.Subsystem.GroundIntake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
+
 public class HatchGrabber extends Subsystem {
-  private WPI_TalonSRX groundmotor;
-  //DigitalInput bottomLimitSwitch; 
-  //DigitalInput topLimitSwitch; 
-  
-    public HatchGrabber(){
-      
-    groundmotor = new WPI_TalonSRX(RobotMap.TALON_GROUND_HATCH);
-    //groundmotor.setNeutralMode(WPI_TalonSRX.motorcontrol.setNeutralMode.Brake);
-    }
-    // public void Init(){
-  
-    //   bottomLimitSwitch = new DigitalInput(1);
-    //   topLimitSwitch = new DigitalInput(2);
-  
-    //}
-  
-    public void setpower(double power){
-  
-      groundmotor.set(ControlMode.PercentOutput, power);
-  
-    }
-  
-    // public void setlimit(boolean limit, boolean upOrDown){
-      
-    //   if(upOrDown = true){
-  
-    //     limit = topLimitSwitch.get(); // *possibly incorrect?
-  
-    //   }else{
-  
-    //     limit = bottomLimitSwitch.get(); // *possibly incorrect?
-  
-    //   }
-  
-    // }
-  
-    @Override
-    public void initDefaultCommand() {
-      // Set the default command for a subsystem here.
-      // setDefaultCommand(new MySpecialCommand());
-    }
+private WPI_VictorSPX groundmotor;
+
+  public HatchGrabber(){
+    //groundmotor = new WPI_VictorSPX(RobotMap.VICTOR_GROUND_HATCH);
+    groundmotor.setNeutralMode(com.ctre.phoenix.motorcontrol.NeutralMode.Brake);
   }
+
+  public void setpower(double power){
+
+    groundmotor.set(ControlMode.PercentOutput, power);
+
+  }
+
+    
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
--- src/main/java/frc/robot/Subsystem/Intake/Sensors.java
+++ src/main/java/frc/robot/Subsystem/Intake/Sensors.java
@@ -1,13 +1,22 @@
-package frc.robot.Subsystem.Intake;
+// package frc.robot.Subsystem.Intake;
 
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.command.Subsystem;
+// import edu.wpi.first.wpilibj.I2C;
+// import edu.wpi.first.wpilibj.command.Subsystem;
+// includeSensorBar.h
 
+// public class Sensors extends Subsystem {
+// public I2C sensor1;
+// public array buff1;
+// public SensorBar MySensorBar(0x3E);
+// public Sensors(){
 
-public class Sensors extends Subsystem {
+//   sensor1.read(0x3E, 6, buff1);
+//   sensor1.read(0x3F, 6, buff1);
+//   sensor1.read(0x70, 6, buff1);
+//   sensor1.read(0x71, 6, buff1);
+// }
 
-
-  @Override
-  public void initDefaultCommand() {
-  }
-}
+//   @Override
+//   public void initDefaultCommand() {
+//   }
+// }
