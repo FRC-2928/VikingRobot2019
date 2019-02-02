3d820a69942c3fad0f9e4c5ca21db071d4042626
matthewpanc <44793372+matthewpanc@users.noreply.github.com>
2019-02-02 14:46:32 -0800

    more arm preset
    some pseduo code place holders
    
    Co-Authored-By: Erika Piltz <erikatatiana@users.noreply.github.com>

--- src/main/java/frc/robot/Command/Intake/armPreSet.java
+++ src/main/java/frc/robot/Command/Intake/armPreSet.java
@@ -7,20 +7,23 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class armPreSet extends Command{
     int midpoint= 0; //set
-    public void hatchSet(int setMidPoint){/*
-        i = (hatchDistance in ticks)/2
-        
-        RunLeftThreadbar(midPoint-i)
-        RunRightThreadbar(midPoint+i)
+    public void hatchSet(int moveAmount){/*
+        i = (differance needed between bars)/2;
+        if arms close{ do nothing }
+        else:
+            robot.Subsystem.Intake.LeftThreadbar(moveAmount-i,0.5,0.5);;
+            robot.Subsystem.Intake.RightThreadbar(moveAmount+i,0.5,0.5);;
         */
     }
-    public void ballSet(int setMidPoint){/*
-        o = (ballDistance in ticks)/2
-        RunLeftThreadbar(midPoint-o)
-        RunRightThreadbar(midPoint+o)
+    public void ballSet(int moveAmount){/*
+        o = (differance needed between bars)/2; 
+        if arms far{do nothing}
+        else:
+            robot.Subsystem.Intake.LeftThreadbar(moveAmount-o,0.5,0.5);;
+            robot.Subsystem.Intake.RightThreadbar(moveAmount+o,0.5,0.5);;
         */
     }
     protected boolean isFinished() {
-        return false;
+        return true;
       }
 }
